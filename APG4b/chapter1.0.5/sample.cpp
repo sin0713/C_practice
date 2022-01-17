2f5ca6cbe15c9e5139c9925ea2384b8b0a78d956
sin0713 <narisin1124@gmail.com>
2022-01-17 22:32:42 +0900

    [Add]chapter5

---
+++ APG4b/chapter1.0.5/sample.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int name;
+
+  name = 10;
+
+  cout << name << endl;
+  cout << name + 2 << endl;
+  cout << name * 3 << endl;
+  cout << "-----------------" << endl;
+
+  int a = 10, b = 5;
+
+  cout << a << endl;
+  cout << b << endl;
+}
