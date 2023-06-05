683845a28725c9047927fdd911c5b9553b3e3a78
sin0713 <narisin1124@gmail.com>
2023-06-05 22:29:09 +0900

    アルゴリズム再入門

---
+++ algorythm/code4_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int func(int n) {
+  cout << "func(" << n << ")を呼び出し" << endl;
+
+  if (n == 0) return 0;
+
+  int result = n + func(n - 1);
+  cout << n << "までの総和" << result << endl;
+
+  return result;
+}
+
+int main() {
+  func(5);
+
+}
