a34fc2397ae818a76aa6f4bd982562d801dd4bf0
sin0713 <narisin1124@gmail.com>
2023-06-09 08:22:40 +0900

    動的計画法 別海

--- algorythm/chapter5/code5_1.cpp
+++ algorythm/chapter5/code5_1.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 const long long INF = 1LL << 60;
 
+template<class T> void chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+  }
+}
+
 int main() {
   int N;
   cin >> N;
@@ -12,17 +18,12 @@ int main() {
   for (int i = 0; i < N; i++) cin >> heights[i];
 
   vector<long long> dp(N, INF);
+  dp[0] = 0;
 
-  for (int i = 0; i < N; i++) {
-    if (i == 0) {
-      dp[i] = 0;
-    } else if (i == 1) {
-      dp[i] = dp[i - 1] + heights[i];
-    } else {
-      int dp1 = dp[i - 1] + abs(heights[i] - heights[i - 1]);
-      int dp2 = dp[i - 2] + abs(heights[i] - heights[i - 2]);
-
-      dp[i] = min(dp1, dp2);
+  for (int i = 1; i < N; i++) {
+    chmin(dp[i], dp[i - 1] + abs(heights[i] - heights[i - 1]));
+    if (i > 1) {
+      chmin(dp[i], dp[i - 2] + abs(heights[i] - heights[i - 2]));
     }
   }
 
