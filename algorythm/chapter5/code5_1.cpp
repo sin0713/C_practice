fac45016e8536bad056adecee6d7d3730537ce39
sin0713 <narisin1124@gmail.com>
2023-06-10 08:33:13 +0900

    動的計画法 再帰

--- algorythm/chapter5/code5_1.cpp
+++ algorythm/chapter5/code5_1.cpp
@@ -4,28 +4,36 @@ using namespace std;
 
 const long long INF = 1LL << 60;
 
+int N;
+vector<long long> dp;
+vector<int> heights;
+
 template<class T> void chmin(T& a, T b) {
   if (a > b) {
     a = b;
   }
 }
 
+long long find(int i) {
+  if (dp[i] < INF) return dp[i];
+  if (i == 0) return 0;
+
+  chmin(dp[i], find(i - 1) + abs(heights[i] - heights[i - 1]));
+  if (i > 1) chmin(dp[i], find(i - 2) + abs(heights[i] - heights[i - 2]));
+
+  return dp[i];
+}
+
 int main() {
-  int N;
   cin >> N;
 
-  vector<int> heights(N);
+  heights.resize(N);
   for (int i = 0; i < N; i++) cin >> heights[i];
 
-  vector<long long> dp(N, INF);
+  dp.assign(N, INF);
   dp[0] = 0;
 
-  for (int i = 1; i < N; i++) {
-    chmin(dp[i], dp[i - 1] + abs(heights[i] - heights[i - 1]));
-    if (i > 1) {
-      chmin(dp[i], dp[i - 2] + abs(heights[i] - heights[i - 2]));
-    }
-  }
+  find(N - 1);
 
   cout << dp[N -1] << endl;
 }
